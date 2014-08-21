52e6d897cf07420091e6d91f15cf216db2e5bd9c
Kevin Gibbons <bakkot@gmail.com>
2014-08-20 19:56:19 -0700

    bugfixing

--- ssc.js
+++ ssc.js
@@ -1,120 +1,15 @@
-// ==UserScript==
-// @name           SlateStarComments
-// @namespace      http://www.github.com/bakkot
-// @description    Highlight new comments on Slate Star Codex posts
-// @match          http://slatestarcodex.com/20*
-// @version        1.2
-// ==/UserScript==
+// run to reset:
+// localStorage.removeItem('visited-' + location.pathname);
 
 
 // Global variables are fun!
-var lastGivenDate;
-
-
-// *** Inject some css used by the floating list
-
-var styleEle = document.createElement('style');
-styleEle.type = 'text/css';
-styleEle.innerHTML = '.new-comment { border: 2px solid #5a5; }' +
-'.comments-floater { position: fixed; right: 4px; top: 4px; padding: 2px 5px; width: 230px;font-size: 14px; border-radius: 5px; background: rgba(250, 250, 250, 0.90); }' +
-'.comments-scroller { word-wrap: break-word; max-height: 500px; overflow-y:scroll; }' +
-'.comments-date { font-size: 11px; }' +
-'.semantic-cell { display: table-cell; }' +
-'.cct-span { white-space: nowrap; }' +
-'.date-input { width: 100%; box-sizing: border-box; }' +
-'.input-span { width: 100%; padding-left: 5px; }' +
-'.hider { position: absolute; left: -19px; top: 6px;}' +
-'';
-document.head.appendChild(styleEle);
-
-
-
-// *** Create and insert the floating list of comments, and its contents
-
-
-// The floating box.
-var floatBox = document.createElement('div');
-floatBox.className = 'comments-floater';
-
-
-// Container for the text node below.
-var cctSpan = document.createElement('span');
-cctSpan.className = 'semantic-cell cct-span';
-
-// The text node which says 'x comments since'
-var commentCountText = document.createTextNode('');
-
-
-// Container for the text box below.
-var inputSpan = document.createElement('span');
-inputSpan.className = 'semantic-cell input-span';
-
-// The text box with the date.
-var dateInput = document.createElement('input');
-dateInput.className = 'date-input';
-dateInput.addEventListener('blur', function(){
-  var newDate = Date.parse(dateInput.value);
-  if (isNaN(newDate)) {
-    alert('Given date not valid.');
-    dateInput.value = (new Date(lastGivenDate)).toLocaleString();
-    return;
-  }
-  border(newDate, false);
-}, false);
-dateInput.addEventListener('keypress', function(e){
-  if (e.keyCode === 13) {
-  	dateInput.blur();
-  }
-}, false);
-
-
-// Container for the comments list and the '[-]'
-var divDiv = document.createElement('div');
-divDiv.style.display = 'none';
-
-// The '[-]'
-var hider = document.createElement('span');
-hider.innerHTML = '[-]';
-hider.className = 'hider';
-hider.addEventListener('click', function(){
-  if (commentsScroller.style.display != 'none') {
-    commentsScroller.style.display = 'none';
-  }
-  else {
-    commentsScroller.style.display = '';
-  }
-}, false);
-
-// Scrollable container for the comments list 
-var commentsScroller = document.createElement('div');
-commentsScroller.className = 'comments-scroller';
-commentsScroller.style.display = 'none';
-
-// Actual list of comments
-var commentsList = document.createElement('ul');
-
-
-
-// Insert all the things we made into each other and ultimately the document.
-
-cctSpan.appendChild(commentCountText);
-floatBox.appendChild(cctSpan);
-
-inputSpan.appendChild(dateInput);
-floatBox.appendChild(inputSpan);
-
-divDiv.appendChild(hider);
-commentsScroller.appendChild(commentsList);
-divDiv.appendChild(commentsScroller);
-floatBox.appendChild(divDiv);
-
-document.body.appendChild(floatBox);
+var lastGivenDate, commentCountText, commentsList, divDiv, dateInput, commentsScroller;
 
 
 
 
 
-// *** Set up borders and populate comments list
+// *** Sets up borders and populates comments list
 
 function border(since, updateTitle) {
   lastGivenDate = since;
@@ -164,29 +59,7 @@ function border(since, updateTitle) {
 }
 
 
-
-
-// *** Retrieve the last-visit time from storage, border all comments made after, and save the time of the latest comment in storage for next time 
-
-var pathString = 'visited-' + location.pathname;
-var lastVisit = parseInt(localStorage[pathString]);
-if (isNaN(lastVisit)) {
-  lastVisit = 0; // prehistory! Actually 1970, which predates all SSC comments, so we're good.
-}
-dateInput.value = (new Date(lastVisit)).toLocaleString();
-var mostRecent = border(lastVisit, false);
-localStorage[pathString] = mostRecent;
-
-
-// run to reset:
-// localStorage.removeItem('visited-' + location.pathname);
-
-
-
-
-
-
-// *** Add buttons to show/hide threads
+// *** Toggles visibility of comment which invoked it
 
 function commentToggle() {
   var myComment = this.parentElement.parentElement;
@@ -215,18 +88,154 @@ function commentToggle() {
 }
 
 
-var comments = document.querySelectorAll('div.commentholder');
 
-for(var i=0; i<comments.length; ++i) {
-  var hideLink = document.createElement('a');
-  hideLink.className = 'comment-reply-link';
-  hideLink.style.textDecoration = 'underline';
-  hideLink.innerHTML = 'Hide';
 
-  hideLink.addEventListener('click', commentToggle, false);
 
-  var divs = comments[i].children;
-  var replyEle = divs[divs.length-1];
+// ** Set up highlights on first run
 
-  replyEle.appendChild(hideLink);
+function makeHighlight() {
+  // *** Inject some css used by the floating list
+
+  var styleEle = document.createElement('style');
+  styleEle.type = 'text/css';
+  styleEle.innerHTML = '.new-comment { border: 2px solid #5a5; }' +
+  '.comments-floater { position: fixed; right: 4px; top: 4px; padding: 2px 5px; width: 230px;font-size: 14px; border-radius: 5px; background: rgba(250, 250, 250, 0.90); }' +
+  '.comments-scroller { word-wrap: break-word; max-height: 500px; overflow-y:scroll; }' +
+  '.comments-date { font-size: 11px; }' +
+  '.semantic-cell { display: table-cell; }' +
+  '.cct-span { white-space: nowrap; }' +
+  '.date-input { width: 100%; box-sizing: border-box; }' +
+  '.input-span { width: 100%; padding-left: 5px; }' +
+  '.hider { position: absolute; left: -19px; top: 6px;}' +
+  '';
+  document.head.appendChild(styleEle);
+
+
+
+  // *** Create and insert the floating list of comments, and its contents
+
+
+  // The floating box.
+  var floatBox = document.createElement('div');
+  floatBox.className = 'comments-floater';
+
+
+  // Container for the text node below.
+  var cctSpan = document.createElement('span');
+  cctSpan.className = 'semantic-cell cct-span';
+
+  // The text node which says 'x comments since'
+  commentCountText = document.createTextNode('');
+
+
+  // Container for the text box below.
+  var inputSpan = document.createElement('span');
+  inputSpan.className = 'semantic-cell input-span';
+
+  // The text box with the date.
+  dateInput = document.createElement('input');
+  dateInput.className = 'date-input';
+  dateInput.addEventListener('blur', function(){
+    var newDate = Date.parse(dateInput.value);
+    if (isNaN(newDate)) {
+      alert('Given date not valid.');
+      dateInput.value = (new Date(lastGivenDate)).toLocaleString();
+      return;
+    }
+    border(newDate, false);
+  }, false);
+  dateInput.addEventListener('keypress', function(e){
+    if (e.keyCode === 13) {
+      dateInput.blur();
+    }
+  }, false);
+
+
+  // Container for the comments list and the '[-]'
+  divDiv = document.createElement('div');
+  divDiv.style.display = 'none';
+
+  // The '[-]'
+  var hider = document.createElement('span');
+  hider.innerHTML = '[-]';
+  hider.className = 'hider';
+  hider.addEventListener('click', function(){
+    if (commentsScroller.style.display != 'none') {
+      commentsScroller.style.display = 'none';
+    }
+    else {
+      commentsScroller.style.display = '';
+    }
+  }, false);
+
+  // Scrollable container for the comments list 
+  commentsScroller = document.createElement('div');
+  commentsScroller.className = 'comments-scroller';
+  commentsScroller.style.display = 'none';
+
+  // Actual list of comments
+  commentsList = document.createElement('ul');
+
+
+
+  // Insert all the things we made into each other and ultimately the document.
+
+  cctSpan.appendChild(commentCountText);
+  floatBox.appendChild(cctSpan);
+
+  inputSpan.appendChild(dateInput);
+  floatBox.appendChild(inputSpan);
+
+  divDiv.appendChild(hider);
+  commentsScroller.appendChild(commentsList);
+  divDiv.appendChild(commentsScroller);
+  floatBox.appendChild(divDiv);
+
+  document.body.appendChild(floatBox);
+
+
+
+
+
+  // *** Retrieve the last-visit time from storage, border all comments made after, and save the time of the latest comment in storage for next time 
+
+  var pathString = 'visited-' + location.pathname;
+  var lastVisit = parseInt(localStorage[pathString]);
+  if (isNaN(lastVisit)) {
+    lastVisit = 0; // prehistory! Actually 1970, which predates all SSC comments, so we're good.
+  }
+  dateInput.value = (new Date(lastVisit)).toLocaleString();
+  var mostRecent = border(lastVisit, false);
+  localStorage[pathString] = mostRecent;
+}
+
+
+
+function makeShowHide() {
+  // *** Add buttons to show/hide threads
+
+  var comments = document.querySelectorAll('div.commentholder');
+
+  for(var i=0; i<comments.length; ++i) {
+    var hideLink = document.createElement('a');
+    hideLink.className = 'comment-reply-link';
+    hideLink.style.textDecoration = 'underline';
+    hideLink.innerHTML = 'Hide';
+
+    hideLink.addEventListener('click', commentToggle, false);
+
+    var divs = comments[i].children;
+    var replyEle = divs[divs.length-1];
+
+    replyEle.appendChild(hideLink);
+  }
+}
+
+
+
+
+// Run iff we're on a page which looks like a post
+if(location.pathname.substring(0, 3) == '/20') {
+  makeHighlight();
+  makeShowHide();
 }
