a08f257d37abc063ae74258ba1930559ed7511b1
Kevin Gibbons <bakkot@gmail.com>
2014-12-09 15:24:48 -0800

    parent link script

---
+++ sscparents.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name           SlateStarComments Parents
+// @namespace      http://www.github.com/bakkot
+// @description    Adds link to parent comment on SSC threads
+// @match          http://slatestarcodex.com/20*
+// @version        0.1
+// ==/UserScript==
+
+
+function makeParentLinks() {
+  var commentList = document.querySelectorAll('li.comment');
+  for(var i = 0; i < commentList.length; ++i) {
+    if(commentList[i].parentElement.tagName == 'OL') {
+      continue;
+    }
+    
+    var parent = commentList[i].parentElement.parentElement;
+    var parentID = parent.firstElementChild.id;
+
+
+    var parentLink = document.createElement('a');
+    parentLink.href = '#' + parentID;
+    parentLink.className = 'comment-reply-link';
+    parentLink.style.textDecoration = 'underline';
+    parentLink.textContent = '↑';
+
+    var divs = commentList[i].query;
+    var replyEle = commentList[i].querySelector('div.reply');
+    
+    replyEle.appendChild(document.createTextNode(' '));
+
+    replyEle.appendChild(parentLink);
+    
+  }
+}
+
+makeParentLinks();
